5fb4f28d1bea7f965e7cce24fe2314b8f12c84d8
harun <harunuyan6@gmail.com>
2023-03-11 21:53:00 +0300

    parcelabled model classes

--- app/src/main/java/com/volie/newsapphilt/model/Article.kt
+++ app/src/main/java/com/volie/newsapphilt/model/Article.kt
@@ -1,9 +1,12 @@
 package com.volie.newsapphilt.model
 
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "articles")
 data class Article(
     @PrimaryKey(autoGenerate = true)
@@ -16,4 +19,4 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String?
-)
+) : Parcelable
--- app/src/main/java/com/volie/newsapphilt/model/Source.kt
+++ app/src/main/java/com/volie/newsapphilt/model/Source.kt
@@ -1,7 +1,10 @@
 package com.volie.newsapphilt.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Source(
     val id: String? = "",
     val name: String?
-)
+) : Parcelable
