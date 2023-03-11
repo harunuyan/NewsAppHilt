1bd5455679e84cf4b2cac9ce1e0512f53396e417
harun <harunuyan6@gmail.com>
2023-03-11 17:27:47 +0300

    implemented bottom navigation menu

---
+++ app/src/main/java/com/volie/newsapphilt/view/NewsActivity.kt
@@ -0,0 +1,27 @@
+package com.volie.newsapphilt.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.volie.newsapphilt.R
+import com.volie.newsapphilt.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityNewsBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        mBinding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
