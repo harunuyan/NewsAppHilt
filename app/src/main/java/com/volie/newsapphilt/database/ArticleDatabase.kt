7b285ceb54ac44dac820480ef76fedf139618090
harun <harunuyan6@gmail.com>
2023-03-11 21:54:15 +0300

    implemented converter class for Source model

--- app/src/main/java/com/volie/newsapphilt/database/ArticleDatabase.kt
+++ app/src/main/java/com/volie/newsapphilt/database/ArticleDatabase.kt
@@ -2,9 +2,11 @@ package com.volie.newsapphilt.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.volie.newsapphilt.model.Article
 
 @Database(entities = [Article::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
 abstract class ArticleDatabase : RoomDatabase() {
     abstract fun articleDao(): ArticleDao
 }
