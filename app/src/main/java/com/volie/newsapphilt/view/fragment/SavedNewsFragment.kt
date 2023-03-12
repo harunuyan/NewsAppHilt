32e238431dfeec79554e1683efed9d41d6a04b54
harun <harunuyan6@gmail.com>
2023-03-12 12:19:08 +0300

    inserted data

--- app/src/main/java/com/volie/newsapphilt/view/fragment/SavedNewsFragment.kt
+++ app/src/main/java/com/volie/newsapphilt/view/fragment/SavedNewsFragment.kt
@@ -5,18 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.volie.newsapphilt.databinding.FragmentSavedNewsBinding
 import com.volie.newsapphilt.view.fragment.adapter.NewsAdapter
+import com.volie.newsapphilt.viewmodel.SavedNewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class SavedNewsFragment
 @Inject constructor() : Fragment() {
     private var _mBinding: FragmentSavedNewsBinding? = null
     private val mBinding get() = _mBinding!!
+    private val mViewModel: SavedNewsViewModel by viewModels()
     private val mAdapter: NewsAdapter by lazy {
         NewsAdapter {
-            val action = SavedNewsFragmentDirections.actionSavedNewsFragment2ToArticleFragment(it)
+            val action = SavedNewsFragmentDirections.actionSavedNewsFragmentToArticleFragment(
+                ArticleFragmentArgs(it).article
+            )
             findNavController().navigate(action)
         }
     }
@@ -28,6 +36,26 @@ class SavedNewsFragment
     ): View? {
         _mBinding = FragmentSavedNewsBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        initObserver()
+    }
+
+
+    private fun setupRecyclerView() {
+        with(mBinding) {
+            rvSavedNews.adapter = mAdapter
+            rvSavedNews.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun initObserver() {
+        mViewModel.getNewsFromLocal().observe(viewLifecycleOwner) {
+            mAdapter.differ.submitList(it)
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/volie/newsapphilt/viewmodel/SavedNewsViewModel.kt
+++ app/src/main/java/com/volie/newsapphilt/viewmodel/SavedNewsViewModel.kt
@@ -1,6 +1,26 @@
 package com.volie.newsapphilt.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.volie.newsapphilt.model.Article
+import com.volie.newsapphilt.repo.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SavedNewsViewModel: ViewModel() {
+@HiltViewModel
+class SavedNewsViewModel
+@Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    fun getNewsFromLocal() = repository.getNewsFromLocal()
+
+    fun deleteArticle(article: Article) {
+        viewModelScope.launch {
+            repository.deleteArticle(article)
+        }
+    }
 }
