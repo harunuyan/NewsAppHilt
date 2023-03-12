c81ff99863c1eb423fd68f18ab4efdf14a2246e4
harun <harunuyan6@gmail.com>
2023-03-12 12:06:00 +0300

    observed live data in BreakingNewsFragment & get data from remote in BreakingNewsViewModel

--- app/src/main/java/com/volie/newsapphilt/view/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/volie/newsapphilt/view/fragment/BreakingNewsFragment.kt
@@ -1,23 +1,31 @@
 package com.volie.newsapphilt.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.volie.newsapphilt.databinding.FragmentBreakingNewsBinding
+import com.volie.newsapphilt.util.Status
 import com.volie.newsapphilt.view.fragment.adapter.NewsAdapter
+import com.volie.newsapphilt.viewmodel.BreakingNewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class BreakingNewsFragment
 @Inject constructor() : Fragment() {
     private var _mBinding: FragmentBreakingNewsBinding? = null
     private val mBinding get() = _mBinding!!
+    private val mViewModel: BreakingNewsViewModel by viewModels()
     private val mAdapter: NewsAdapter by lazy {
         NewsAdapter {
             val action =
-                BreakingNewsFragmentDirections.actionBreakingNewsFragment2ToArticleFragment(it)
+                BreakingNewsFragmentDirections.actionBreakingNewsFragmentToArticleFragment(it)
             findNavController().navigate(action)
         }
     }
@@ -26,14 +34,54 @@ class BreakingNewsFragment
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _mBinding = FragmentBreakingNewsBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAdapter()
+        mViewModel.getBreakingNews()
+        initObserver()
+    }
+
+    private fun setupAdapter() {
+        with(mBinding) {
+            rvBreakingNews.adapter = mAdapter
+            rvBreakingNews.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    fun initObserver() {
+        mViewModel.news.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    it.data?.let { news ->
+                        with(mBinding) {
+                            tvError.visibility = View.GONE
+                            paginationProgressBar.visibility = View.GONE
+                        }
+                        mAdapter.differ.submitList(news.articles)
+                    }
+                }
+                Status.ERROR -> {
+                    with(mBinding) {
+                        tvError.visibility = View.VISIBLE
+                        paginationProgressBar.visibility = View.GONE
+                    }
+                    Log.e("Error!", "${it.message}")
+                }
+                Status.LOADING -> {
+                    mBinding.paginationProgressBar.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _mBinding = null
     }
+
 }
--- app/src/main/java/com/volie/newsapphilt/viewmodel/BreakingNewsViewModel.kt
+++ app/src/main/java/com/volie/newsapphilt/viewmodel/BreakingNewsViewModel.kt
@@ -1,6 +1,29 @@
 package com.volie.newsapphilt.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.volie.newsapphilt.model.News
+import com.volie.newsapphilt.repo.Repository
+import com.volie.newsapphilt.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class BreakingNewsViewModel: ViewModel() {
+@HiltViewModel
+class BreakingNewsViewModel
+@Inject constructor(
+    val repository: Repository
+) : ViewModel() {
+
+    private val _news = MutableLiveData<Resource<News>>()
+    val news: LiveData<Resource<News>> = _news
+
+    fun getBreakingNews() {
+        viewModelScope.launch {
+            val response = repository.getBrekingNewsFromRemote()
+            _news.postValue(response)
+        }
+    }
 }
