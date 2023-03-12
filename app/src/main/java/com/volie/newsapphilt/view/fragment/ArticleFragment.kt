d5709dffb0478992c65d2890f014df6cfa79f87e
harun <harunuyan6@gmail.com>
2023-03-12 12:21:10 +0300

    saved data

--- app/src/main/java/com/volie/newsapphilt/view/fragment/ArticleFragment.kt
+++ app/src/main/java/com/volie/newsapphilt/view/fragment/ArticleFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.volie.newsapphilt.databinding.FragmentArticleBinding
+import com.volie.newsapphilt.viewmodel.ArticleViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class ArticleFragment
 @Inject constructor() : Fragment() {
     private var _mBinding: FragmentArticleBinding? = null
     private val mBinding get() = _mBinding!!
+    private val mViewModel: ArticleViewModel by viewModels()
     val args: ArticleFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -24,8 +31,26 @@ class ArticleFragment
         return mBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val article = args.article
+        mBinding.webView.apply {
+            webViewClient = WebViewClient()
+            try {
+                loadUrl(article.url)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        mBinding.fab.setOnClickListener {
+            mViewModel.insertArticle(article)
+            Snackbar.make(view, "Article saved succesfully", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _mBinding = null
     }
+
 }
--- app/src/main/java/com/volie/newsapphilt/viewmodel/ArticleViewModel.kt
+++ app/src/main/java/com/volie/newsapphilt/viewmodel/ArticleViewModel.kt
@@ -1,6 +1,22 @@
 package com.volie.newsapphilt.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.volie.newsapphilt.model.Article
+import com.volie.newsapphilt.repo.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ArticleViewModel: ViewModel() {
+@HiltViewModel
+class ArticleViewModel
+@Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    fun insertArticle(article: Article) {
+        viewModelScope.launch {
+            repository.insertArticle(article)
+        }
+    }
 }
